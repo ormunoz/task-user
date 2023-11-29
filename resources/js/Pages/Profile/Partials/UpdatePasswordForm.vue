54555c69ff62df4a2c8a00ed99ece30b3213592e
ormunoz <or.munozf@gmail.com>
2023-11-29 01:38:49 -0300

    se adapta codigo a composition y bootstrap

--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
@@ -1,5 +1,44 @@
-<script setup>
-import { ref } from 'vue';
+
+<template>
+
+    <FormSection @submitted="updatePassword">
+        <template #title>
+            Actualizar Contraseña
+        </template>
+
+        <template #description>
+            Aquí puedes actualizar tu contraseña, debes ingresar la contraseña actual y luego la nueva, la contraseña como requisito debe
+            tener minimo 8 caracteres.
+        </template>
+
+        <template #form>
+            <TextInput v-model="form.current_password" type="password" label="Contraseña actual"
+                placeholder="Contraseña actual" />
+            <InputError class="mt-2" :message="form.errors.current_password" />
+
+            <TextInput v-model="form.password" type="password" label="Nueva Contraseña" placeholder="Nueva Contraseña" />
+            <InputError class="mt-2" :message="form.errors.password" />
+
+            <TextInput v-model="form.password_confirmation" type="password" label="Repita Contraseña "
+                placeholder="Repita Contraseña" />
+            <InputError class="mt-2" :message="form.errors.password_confirmation" />
+        </template>
+
+        <template #actions>
+            <ActionMessage :on="form.recentlySuccessful" class="me-3 mt-3">
+                Saved.
+            </ActionMessage>
+
+            <PrimaryButton :class="{ 'disabled': form.processing }" :disabled="form.processing">
+                Guardar
+            </PrimaryButton>
+        </template>
+    </FormSection>
+</template>
+
+
+<script >
+import { ref, defineComponent } from 'vue';
 import { useForm } from '@inertiajs/vue3';
 import ActionMessage from '@/Components/ActionMessage.vue';
 import FormSection from '@/Components/FormSection.vue';
@@ -8,93 +47,51 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 
-const passwordInput = ref(null);
-const currentPasswordInput = ref(null);
+export default defineComponent({
+    components: {
+        ActionMessage,
+        FormSection,
+        InputError,
+        InputLabel,
+        PrimaryButton,
+        TextInput,
+    },
 
-const form = useForm({
-    current_password: '',
-    password: '',
-    password_confirmation: '',
-});
+    setup() {
+        const passwordInput = ref(null);
+        const currentPasswordInput = ref(null);
 
-const updatePassword = () => {
-    form.put(route('user-password.update'), {
-        errorBag: 'updatePassword',
-        preserveScroll: true,
-        onSuccess: () => form.reset(),
-        onError: () => {
-            if (form.errors.password) {
-                form.reset('password', 'password_confirmation');
-                passwordInput.value.focus();
-            }
+        const form = useForm({
+            current_password: '',
+            password: '',
+            password_confirmation: '',
+        });
 
-            if (form.errors.current_password) {
-                form.reset('current_password');
-                currentPasswordInput.value.focus();
-            }
-        },
-    });
-};
+        const updatePassword = () => {
+            form.put(route('user-password.update'), {
+                errorBag: 'updatePassword',
+                preserveScroll: true,
+                onSuccess: () => form.reset(),
+                onError: () => {
+                    if (form.errors.password) {
+                        form.reset('password', 'password_confirmation');
+                        passwordInput.value.focus();
+                    }
+
+                    if (form.errors.current_password) {
+                        form.reset('current_password');
+                        currentPasswordInput.value.focus();
+                    }
+                },
+            });
+        };
+
+        return {
+            updatePassword,
+            form
+        }
+    }
+
+})
+
 </script>
-
-<template>
-    <FormSection @submitted="updatePassword">
-        <template #title>
-            Update Password
-        </template>
-
-        <template #description>
-            Ensure your account is using a long, random password to stay secure.
-        </template>
-
-        <template #form>
-            <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="current_password" value="Current Password" />
-                <TextInput
-                    id="current_password"
-                    ref="currentPasswordInput"
-                    v-model="form.current_password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    autocomplete="current-password"
-                />
-                <InputError :message="form.errors.current_password" class="mt-2" />
-            </div>
-
-            <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="password" value="New Password" />
-                <TextInput
-                    id="password"
-                    ref="passwordInput"
-                    v-model="form.password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    autocomplete="new-password"
-                />
-                <InputError :message="form.errors.password" class="mt-2" />
-            </div>
-
-            <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="password_confirmation" value="Confirm Password" />
-                <TextInput
-                    id="password_confirmation"
-                    v-model="form.password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    autocomplete="new-password"
-                />
-                <InputError :message="form.errors.password_confirmation" class="mt-2" />
-            </div>
-        </template>
-
-        <template #actions>
-            <ActionMessage :on="form.recentlySuccessful" class="me-3">
-                Saved.
-            </ActionMessage>
-
-            <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Save
-            </PrimaryButton>
-        </template>
-    </FormSection>
-</template>
